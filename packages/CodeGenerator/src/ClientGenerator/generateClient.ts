625080f29ba4775874d3251f877d2bf1983cdda8
Ben McAlindin <bmcalindin@faculty.ai>
2020-10-20 18:52:58 +0100

    Remove excess files

--- packages/CodeGenerator/src/ClientGenerator/generateClient.ts
+++ packages/CodeGenerator/src/ClientGenerator/generateClient.ts
@@ -1,4 +1,9 @@
+import { promises as fs } from 'fs';
+import * as path from 'path';
+
 import { Widget, Layout, Page, ElementTreeNode } from '@ui-builder/types';
+
+import { FilePaths } from '../FilePaths';
 
 import generateLayoutFiles from './layout';
 import generateReducerFiles from './reducers';
@@ -17,6 +22,30 @@ interface Args {
   layouts: Layout[];
 }
 
+interface IRemoveExcessFiles {
+  widgets: Widget[];
+  pages: Page[];
+  layouts: Layout[];
+}
+
+const removeExcessFiles = async ({ widgets, layouts, pages }: IRemoveExcessFiles) => {
+  const componentNames = [...widgets.map((w) => w.name), ...layouts.map((l) => `${l.name}.js`)];
+  const componentFiles = await fs.readdir(FilePaths.components);
+  await Promise.all(
+    componentFiles
+      .filter((c) => !componentNames.includes(c))
+      .map((c) => fs.unlink(path.join(FilePaths.components, c))),
+  );
+
+  const pageNames = pages.map((p) => `${p.name}.js`);
+  const pageFiles = await fs.readdir(FilePaths.pages);
+  await Promise.all(
+    pageFiles
+      .filter((p) => !pageNames.includes(p))
+      .map((p) => fs.unlink(path.join(FilePaths.pages, p))),
+  );
+};
+
 const generateClient = async ({ elementTree, widgets, pages, layouts }: Args) => {
   return Promise.all([
     generateLayoutFiles(layouts),
@@ -28,6 +57,7 @@ const generateClient = async ({ elementTree, widgets, pages, layouts }: Args) =>
     generateSelectorFiles(),
     generateActionFiles(),
     generateCoreFiles(),
+    removeExcessFiles({ widgets, pages, layouts }),
   ]);
 };
 
