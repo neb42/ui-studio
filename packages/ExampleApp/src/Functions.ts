d8f9a37bd8abe3163cc9a829cfd742d093c3a905
Ben McAlindin <bmcalindin@faculty.ai>
2020-12-24 13:23:09 +0000

    Parse function types

--- packages/ExampleApp/src/Functions.ts
+++ packages/ExampleApp/src/Functions.ts
@@ -1,25 +1,42 @@
+import "reflect-metadata";
+
 const Function = (): any => {
-  return (target: Functions, propertyKey: string, descriptor: PropertyDescriptor) => {
+  return (target: any, propertyName: string, descriptor: PropertyDescriptor) => {
     if (!target.registered) target.registered = [];
-    target.registered.push({ name: propertyKey, args: [] });
+
+    const functionString: string = target[propertyName].toString();
+    const regex = new RegExp('^' + propertyName + '\\((.*)\\) {');
+    const paramNames = functionString.match(regex)[1].replace(/ /g, '').split(',');
+
+    const returnType = Reflect.getMetadata('design:returntype', target, propertyName)
+    const paramTypes = Reflect.getMetadata('design:paramtypes', target, propertyName)
+    target.registered.push({
+      name: propertyName,
+      returnType: returnType.name.toLowerCase(), 
+      args: paramTypes.map((p, i) => ({
+        name: paramNames[i],
+        type: p.name.toLowerCase(),
+      })),
+    });
   };
 };
 
 interface IRegistered {
   name: string;
-  args: string[][];
+  returnType: 'string' | 'number' | 'boolean';
+  args: { name: string, type: 'string' | 'number' | 'boolean' }[];
 }
 
 export class Functions {
   registered: IRegistered[];
 
   @Function()
-  public foo = (w_widget1) => {
+  foo(w_widget1: boolean, arg2: string): number {
     return 10;
   };
 
   @Function()
-  public bar = (w_widget1) => {
+  bar(w_widget1: number): string {
     return 'Hello world';
   };
 }
