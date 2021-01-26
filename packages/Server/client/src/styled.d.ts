0ffc8763f828b00b8ada05de21de7da22b0fa580
Ben McAlindin <bmcalindin@faculty.ai>
2021-01-26 20:39:13 +0000

    Linting

--- packages/Server/client/src/styled.d.ts
+++ packages/Server/client/src/styled.d.ts
@@ -2,5 +2,6 @@ import 'styled-components';
 import { AdlerTheme } from '@faculty/adler-tokens';
 
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface DefaultTheme extends AdlerTheme {}
 }
