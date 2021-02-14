97da69c51b89b55ad06191f193205439c45c1ead
Ben McAlindin <bmcalindin@faculty.ai>
2021-02-14 11:00:20 +0000

    Linting rule

--- packages/Server/client/.eslintrc.js
+++ packages/Server/client/.eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'flowtype/no-types-missing-file-annotation': 0,
     'import/prefer-default-export': 0,
     'react/jsx-filename-extension': 0,
+    'react/require-default-props': 0,
     'import/extensions': [
       'error',
       'ignorePackages',
