0a5957d3ea9a03ca7ca36f48c89ea6eed64953ba
Ben McAlindin <bmcalindin@faculty.ai>
2021-02-26 09:10:45 +0000

    Linting

--- packages/server/client/src/components/PopoverNavigation/PopoverNavigation.tsx
+++ packages/server/client/src/components/PopoverNavigation/PopoverNavigation.tsx
@@ -25,14 +25,24 @@ export const PopoverNavigation = (): JSX.Element => {
   const dispatch = useDispatch();
   const view = useSelector(getSelectedView);
 
-  // const handleSelectView = (v: 'preview' | 'variable' | 'css') => () => dispatch(selectView(v));
-  const handleSelectView = (event: React.ChangeEvent<HTMLInputElement>) => dispatch(selectView(event.target.value as 'preview' | 'variable' | 'css'));
+  const handleSelectView = (event: React.ChangeEvent<HTMLInputElement>) =>
+    dispatch(selectView(event.target.value as 'preview' | 'variable' | 'css'));
 
   return (
     <>
       <Styles.SegmentedControl onChange={handleSelectView}>
-        <Styles.HiddenRadio type="radio" value="preview" id="preview" checked={view === 'preview'} />
-        <Styles.HiddenRadio type="radio" value="variable" id="variable" checked={view === 'variable'} />
+        <Styles.HiddenRadio
+          type="radio"
+          value="preview"
+          id="preview"
+          checked={view === 'preview'}
+        />
+        <Styles.HiddenRadio
+          type="radio"
+          value="variable"
+          id="variable"
+          checked={view === 'variable'}
+        />
         <Styles.HiddenRadio type="radio" value="css" id="css" checked={view === 'css'} />
         <Styles.Label htmlFor="preview">Preview</Styles.Label>
         <Styles.Label htmlFor="variable">Variables</Styles.Label>
