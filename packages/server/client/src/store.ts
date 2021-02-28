4b0ddd96ae8b79460f9706df007ba93cf7da8af8
Ben McAlindin <bmcalindin@faculty.ai>
2021-02-28 15:10:11 +0000

    Redux dev tools trace

--- packages/server/client/src/store.ts
+++ packages/server/client/src/store.ts
@@ -4,7 +4,9 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const composeEnhancers =
-  (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+  (window &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })) ||
+  compose;
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
