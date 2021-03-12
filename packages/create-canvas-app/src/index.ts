25ca2d81bb59dd83e175d96cf5ac41f668bccccb
Ben McAlindin <bmcalindin@faculty.ai>
2021-03-12 22:04:04 +0000

    Improve cca script

--- packages/create-canvas-app/src/index.ts
+++ packages/create-canvas-app/src/index.ts
@@ -3,60 +3,30 @@ import * as path from 'path';
 import { execSync } from 'child_process';
 
 import { copySync } from 'fs-extra';
-import axios from 'axios';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 import * as Mustache from 'mustache';
 
-const getPackageVersion = async (key: string) => {
-  return {
-    name: key,
-    version: 'latest',
-    last: false,
-  };
-  const { data } = await axios.get(`https://registry.npmjs.org/${key}`);
-  return {
-    name: key,
-    version: data['dist-tags'].latest,
-    last: false,
-  };
+const getTemplatePackageKeys = (templates: string[]): [string[], string[]] => {
+  const allPackages = [];
+  const componentPackages = [];
+  templates.forEach((t): void => {
+    switch (t) {
+      case 'faculty': {
+        allPackages.push('@faculty/adler-web-components');
+        allPackages.push('@faculty/adler-canvas-wrapper');
+        componentPackages.push('@faculty/adler-canvas-wrapper');
+        break;
+      }
+      default:
+        break;
+    }
+  });
+  return [allPackages, componentPackages];
 };
 
 const renderPackageJson = async (name: string, directory: string, templates: string[]) => {
-  const templatePackageKeys = (() => {
-    const keys = [];
-    templates.forEach((t): void => {
-      switch (t) {
-        case 'faculty': {
-          keys.push('@faculty/adler-web-components');
-          keys.push('@faculty/adler-canvas-wrapper');
-          break;
-        }
-        default:
-          break;
-      }
-    });
-    return keys;
-  })();
-
-  const dependencies = await Promise.all(
-    ['canvas-typescript', ...templatePackageKeys].map(getPackageVersion),
-  );
-  if (dependencies.length > 0) dependencies[dependencies.length - 1].last = true;
-
-  const devDependencies = await Promise.all(
-    ['canvas-server', 'canvas-types'].map(getPackageVersion),
-  );
-  [
-    { name: 'typescript', version: '^4.0.3', last: false },
-    { name: '@types/node', version: '^12.0.0', last: false },
-    { name: '@types/react', version: '^16.9.53', last: false },
-    { name: '@types/react-dom', version: '^16.9.8', last: false },
-  ].forEach((d) => {
-    devDependencies.push(d);
-  });
-  devDependencies[devDependencies.length - 1].last = true;
-  if (devDependencies.length > 0) devDependencies[devDependencies.length - 1].last = true;
+  const [_, templatePackageKeys] = getTemplatePackageKeys(templates);
 
   const componentPackages = templatePackageKeys.map((t) => ({ name: t, last: false }));
   if (componentPackages.length > 0) componentPackages[componentPackages.length - 1].last = true;
@@ -64,12 +34,48 @@ const renderPackageJson = async (name: string, directory: string, templates: str
   const data = await fs.readFile(path.join(__dirname, 'template', 'package.json.mst'));
   const renderedFile = Mustache.render(data.toString(), {
     name,
-    devDependencies,
-    dependencies,
     componentPackages,
   });
   await fs.writeFile(path.join(directory, 'package.json'), renderedFile);
   await fs.unlink(path.join(directory, 'package.json.mst'));
+};
+
+const initGit = (directory: string) => {
+  execSync('git init', { cwd: directory, stdio: 'inherit' });
+  execSync('git add .', { cwd: directory, stdio: 'inherit' });
+  execSync("git commit -m 'Initialised from create-canvas-app'", {
+    cwd: directory,
+    stdio: 'inherit',
+  });
+};
+
+const addPackages = (directory: string, templates: string[]) => {
+  const [templatePackageKeys] = getTemplatePackageKeys(templates);
+
+  const dependencies = ['canvas-typescript', ...templatePackageKeys].map((k) => ({
+    name: k,
+    version: 'latest',
+  }));
+  const devDependencies = [
+    { name: 'canvas-server', version: 'latest' },
+    { name: 'canvas-types', version: 'latest' },
+    { name: 'typescript', version: '^4.0.3' },
+    { name: '@types/node', version: '^12.0.0' },
+    { name: '@types/react', version: '^16.9.53' },
+    { name: '@types/react-dom', version: '^16.9.8' },
+  ];
+
+  const depsCommand = `yarn add ${dependencies.map((d) => `${d.name}@${d.version}`).join(' ')}`;
+  const devDepsCommand = `yarn add -D ${devDependencies
+    .map((d) => `${d.name}@${d.version}`)
+    .join(' ')}`;
+
+  if (dependencies.length > 0) {
+    execSync(depsCommand, { cwd: directory, stdio: 'inherit' });
+  }
+  if (devDependencies.length > 0) {
+    execSync(devDepsCommand, { cwd: directory, stdio: 'inherit' });
+  }
 };
 
 const run = async (): Promise<void> => {
@@ -92,12 +98,8 @@ const run = async (): Promise<void> => {
 
   await renderPackageJson(name, directory, templates);
 
-  execSync('git init', { cwd: directory, stdio: 'inherit' });
-  execSync("git commit -m 'Initialised from create-canvas-app'", {
-    cwd: directory,
-    stdio: 'inherit',
-  });
-  execSync('yarn', { cwd: directory, stdio: 'inherit' });
+  addPackages(directory, templates);
+  initGit(directory);
 };
 
 run();
