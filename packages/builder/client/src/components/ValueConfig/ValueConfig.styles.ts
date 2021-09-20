f85323a34e2a7379aa62a51bc85b8ae7f4675e69
Ben McAlindin <bmcalindin@faculty.ai>
2021-09-20 11:07:24 +0100

    Styling

--- packages/builder/client/src/components/ValueConfig/ValueConfig.styles.ts
+++ packages/builder/client/src/components/ValueConfig/ValueConfig.styles.ts
@@ -1,7 +1,45 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  border: 1px solid #000;
+  border-radius: 8px;
+  margin-bottom: 16px;
+  position: relative;
+  padding: 16px;
 
-export const Name = styled.span``;
+  & .MuiSpeedDial-root {
+    top: 16px;
+    right: 16px;
+  }
+`;
 
-export const ValueItem = styled.span``;
+export const Name = styled.span`
+  margin-bottom: 8px;
+`;
+
+export const ValueItem = styled.span<{ root?: boolean; direction: 'row' | 'column' }>`
+  display: flex;
+  ${({ direction }) =>
+    direction === 'row'
+      ? css`
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: flex-start;
+          gap: 24px;
+        `
+      : css`
+          flex-direction: column;
+          gap: 8px;
+        `}
+
+  ${({ root }) =>
+    root
+      ? css`
+          border-left: 6px solid #00000025;
+          padding: 4px;
+        `
+      : ''}
+`;
