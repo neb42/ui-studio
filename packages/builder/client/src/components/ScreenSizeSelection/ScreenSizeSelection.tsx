bf96073fcc648e56e42e3b97a55bde6553530aec
Ben McAlindin <bmcalindin@gmail.com>
2021-10-20 15:11:55 +0100

    Change button colors

--- packages/builder/client/src/components/ScreenSizeSelection/ScreenSizeSelection.tsx
+++ packages/builder/client/src/components/ScreenSizeSelection/ScreenSizeSelection.tsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useTheme } from 'styled-components';
 import IconButton from '@mui/material/IconButton';
 import { DesktopMac, LaptopMac, TabletMac, PhoneIphone } from '@mui/icons-material';
-
 import { updatePreviewSize } from 'actions/view';
 import { getPreviewSize } from 'selectors/view';
 import { ScreenSize } from 'types/store';
@@ -20,11 +19,11 @@ export const ScreenSizeSelection = () => {
   };
 
   const getColor = (size: ScreenSize) =>
-    size === previewSize ? theme.palette.primary.main : theme.colors.secondary500;
+    size === previewSize ? theme.palette.info.light : theme.palette.info.main;
 
   return (
     <Styles.Container>
-      <IconButton onClick={handleScreenSizeChange('monitor')} size="large">
+      <IconButton onClick={handleScreenSizeChange('monitor')} size="large" color="info">
         <DesktopMac style={{ color: getColor('monitor') }} />
       </IconButton>
       <IconButton onClick={handleScreenSizeChange('laptop')} size="large">
