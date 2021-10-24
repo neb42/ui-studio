76947c4c60d6a7ebf2c90caf8a6b4457f71a24e3
Ben McAlindin <bmcalindin@gmail.com>
2021-10-24 11:20:59 +0100

    Remove commented code

--- packages/builder/client/src/components/Header/Header.styles.ts
+++ packages/builder/client/src/components/Header/Header.styles.ts
@@ -7,13 +7,6 @@ export const Container = styled.div`
   grid-template-columns: 1fr 1fr 1fr;
   padding: 0 16px;
   border-bottom: 1px solid ${({ theme }) => theme.palette.info.main};
-  /* background-image: linear-gradient(
-    to bottom left,
-    ${({ theme }) => theme.palette.primary.light}80,
-    #fff
-  ); */
-  /* background: linear-gradient(to left, #03001e, #7303c0, #ec38bc, #fdeff9); */
-  /* background: linear-gradient(to left, #8364e8, #d397fa); */
   background: linear-gradient(to left, ${deepPurple[500]}, ${deepPurple[100]});
 
   & > *:nth-child(2) {
